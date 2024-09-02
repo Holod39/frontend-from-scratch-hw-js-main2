d210de87509d429af1e4204a066bc995b42ce560
Holod39 <nomad41pk@mail.ru>
2024-09-02 23:01:16 +0200

    proba

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,9 +7,9 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 = [1,2,3,4,5,6] // your code
-let dice2 = [1,2,3,4,5,6]     // your code
-let isWinningDouble = true  // your code
+let dice1 = Math.floor(Math.random()*6)+1; // your code
+let dice2 = Math.floor(Math.random()*6)+1;  // your code
+let isWinningDouble = ( dice1 === dice2) &&(dice1 > 3); // your code
 
 /*
 console.log('Первый бросок: ' + dice1)
