36d62e40adcc812e60988be4eb0da3db30973092
Holod39 <nomad41pk@mail.ru>
2024-09-02 22:48:30 +0200

    zanovo

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,8 +7,8 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 = Math.floor(Math.random()*6) +1// your code
-let dice2 = Math.floor(Math.random()*6) +1   // your code
+let dice1 = [1,2,3,4,5,6] // your code
+let dice2 = [1,2,3,4,5,6]     // your code
 let isWinningDouble = true  // your code
 
 /*
