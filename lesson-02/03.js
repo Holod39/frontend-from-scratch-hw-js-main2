ea2d3cdca8021d509b56ffdaa575f83eff807679
Holod39 <nomad41pk@mail.ru>
2024-09-03 22:43:45 +0200

    2-3

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -6,16 +6,16 @@
 
 const score = 70 // тестовое значение, можно изменять
 let grade
-if (score >= 0 && score < 50) {
-    grade = 'F';
-} else (score >= 50 && score < 69){
+if (score >= 90 && score <= 100) {
+    grade = 'A';
+} else if (score >= 80 && score < 90) {
+    grade = 'B';
+} else if (score >= 70 && score < 80) {
+    grade = 'C';
+} else if (score >= 50 && score < 70) {
     grade = 'D';
-} if (score >=70 && score < 79) {
-    grade = 'C'
-}else if (score >= 80 && score < 89){
-grade = 'B'
-} else if (score >= 90 && score < 100){
-    grade = 'A'
-}
-
-// your code
+} else if (score >= 0 && score < 50) {
+    grade = 'F';
+} else {
+    grade = 'Некорректный балл'; // для баллов вне диапазона
+}// your code
