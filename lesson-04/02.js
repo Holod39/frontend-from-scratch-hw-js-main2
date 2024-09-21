3b7af6e2b441db5b77f8d9f1700d9164a408b15c
Holod39 <nomad41pk@mail.ru>
2024-09-21 23:32:11 +0200

    zaebloOpyt

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -15,7 +15,7 @@
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findUniqueElements(array) {
+function findUniqueElements(arr) {
     const uniqueElements = [];
     
     for (let i = 0; i < arr.length; i++) {
